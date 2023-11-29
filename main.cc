5fac4c2ac6f271382265c185c45149b84faf52c1
nik7273 <devrajn@umich.edu>
2023-11-29 11:14:29 -0500

    extra

--- main.cc
+++ main.cc
@@ -2,21 +2,26 @@
 #include "color.h"
 #include "ray.h"
 
-
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
-    return color(0,0,0);
+    if (hit_sphere(point3(0,0,-10), 0.5, r)) return color(1, 0, 0);
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a) * color(1.0,1.0,1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
-
-    // PPM formatted image
-    // Image dimensions
-    // for initial "hello world" version
-    // int image_width = 256;
-    // int image_height = 256;
-
     // Image
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
@@ -49,7 +54,6 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            // auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
