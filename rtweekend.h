41f36d2e005bbb8c3e8b9b685ab4c7e015b3be59
devrajn <devrajn@umich.edu>
2024-01-24 17:54:44 -0500

    rng

--- rtweekend.h
+++ rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -18,6 +19,25 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
+
+inline double random_double() {
+    // returns random double in [0,1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // returns random double in [min, max)
+    return min + (max-min)*random_double();
+}
+
+// alternate random double implementation
+// #include <random>
+
+// inline double random_double() {
+//     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+//     static std::mt19937 generator;
+//     return distribution(generator);
+// }
 
 // Common headers
 #include "interval.h"
