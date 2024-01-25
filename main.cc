d83a9dbec5cc2578478028840efa8b14ba068907
devrajn <devrajn@umich.edu>
2024-01-25 17:18:47 -0500

    antialiasing

--- main.cc
+++ main.cc
@@ -9,13 +9,14 @@ int main() {
     // Image
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
+    int samples_per_pixel = 100;
 
     // World
     hittable_list world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
-    camera cam(aspect_ratio, image_width);
+    camera cam(aspect_ratio, image_width, samples_per_pixel);
 
     cam.render(world);
 }
