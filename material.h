8925c5c1741d9848cee6886fe4b8eeab5461662e
devrajn <devrajn@umich.edu>
2024-01-26 17:32:59 -0500

    fuzzy metalsgit add .

--- material.h
+++ material.h
@@ -43,19 +43,20 @@ class lambertian : public material {
 class metal : public material {
     // assuming surface is metal, so it reflects light
     public:
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
         bool scatter(const ray& r_in, const hit_record& rec, 
                     color& attenuation, ray& scattered) const override {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz * random_unit_vector());
             attenuation = albedo;
-            return true; // for now, always scatter with attenuation
+            return (dot(scattered.direction(), rec.normal) > 0); // for now, always scatter with attenuation
         }
 
 
     private:
         color albedo;
+        double fuzz;
 };
 
 #endif
