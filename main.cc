50884dd2f3f331392cfd595553011f49391b7298
devrajn <devrajn@umich.edu>
2024-01-26 19:26:41 -0500

    finished dielectrics

--- main.cc
+++ main.cc
@@ -16,15 +16,25 @@ int main() {
     // World
     hittable_list world;
 
+    // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    // // auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    // // auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    // auto material_center = make_shared<dielectric>(1.5);
+    // auto material_left = make_shared<dielectric>(1.5);
+    // auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left   = make_shared<dielectric>(1.5);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+
 
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, material_ground));
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1,0,-1), 0.5, material_left));    
-    world.add(make_shared<sphere>(point3(1,0,-1), 0.5, material_right));
+    // for the below sphere with negative radius --> surface normal points inward
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0),-0.4, material_left));
+    world.add(make_shared<sphere>(point3(1,0,-1), 0.5, material_right));    
 
     camera cam(aspect_ratio, image_width, samples_per_pixel, max_depth);
 
